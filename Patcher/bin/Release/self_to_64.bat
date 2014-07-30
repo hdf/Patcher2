c9249b7a084cc43b72745591c6cad9285be4b2e1
hdf <hdf@vipmail.hu>
2014-07-30 21:59:14 +0200

    some files got left out (not important)

---
+++ Patcher/bin/Release/self_to_64.bat
@@ -0,0 +1,9 @@
+copy /B /Y patcher2.exe patcher2_64.exe
+>  self_to_64.ini echo Patcher2 32bit to 64bit
+>> self_to_64.ini echo patcher2_64.exe
+>> self_to_64.ini echo 03 00 02 00 2B
+>> self_to_64.ini echo 0
+>> self_to_64.ini echo 1 ? 0
+patcher2 -e self_to_64.ini
+del patcher2_64.exe.bak
+del self_to_64.ini
