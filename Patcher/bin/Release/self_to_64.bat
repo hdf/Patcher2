62902e79debf58162615cca2adae17f432c753d1
hdf <hdf@vipmail.hu>
2014-07-31 01:53:39 +0200

    update2 to self_to_64.bat

--- Patcher/bin/Release/self_to_64.bat
+++ Patcher/bin/Release/self_to_64.bat
@@ -1,7 +1,7 @@
 copy /B /Y patcher2.exe patcher2_64.exe
 >  self_to_64.ini echo Patcher2 32bit to 64bit
 >> self_to_64.ini echo patcher2_64.exe
->> self_to_64.ini echo 03 00 02 00 30
+>> self_to_64.ini echo 03 00 02 00 ? 00 00
 >> self_to_64.ini echo 0
 >> self_to_64.ini echo 1 ? 0
 patcher2 -e self_to_64.ini
